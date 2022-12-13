f71f332d81c88518b965cc008ae267855260ef62
nuha <nohanuha8@gmail.com>
2022-12-13 11:21:04 +0200

    bootstrap project with out footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
+import { fa1 } from '@fortawesome/free-solid-svg-icons';
+import { fa2 } from '@fortawesome/free-solid-svg-icons';
+import { fa3 } from '@fortawesome/free-solid-svg-icons';
+import { faChargingStation } from '@fortawesome/free-solid-svg-icons';
+import { faDisplay } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'project-bootstrap';
+  faMagnifyingGlass = faMagnifyingGlass;
+  faBars = faBars;
+  faPencil = faPencil;
+  fa1 = fa1;
+  fa2 = fa2;
+  fa3 = fa3;
+  faChargingStation = faChargingStation;
+  faDisplay = faDisplay;
 }
